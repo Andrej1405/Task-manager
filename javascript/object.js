5eb6830aec28242f00d9f4cb50b9d83c64dbe08c
Andrey <andrej.udentsov@yandex.ru>
2020-04-02 23:30:38 +0400

    modules and classes

--- javascript/object.js
+++ javascript/object.js
@@ -1,26 +1,21 @@
-let activeProject = [
-    { id: 1, project: "TaskManager", date: '25.03.2020', tasks: [
-        
-    ] },
+let projects = [
+    { id: 1, projectName: "TaskManager", date: '25.03.2020', tasks: [] },
 ],
-    tasksProject = [
-
-    ],
-
     deletedProject = [
 
     ],
 
     complitedProjects = [
-        { id: 2, project: 'Project1', date: '28.03.2020' }
+        { id: 2, projectName: 'Project1', date: '28.03.2020' }
     ];
 
 let employees = [
         { id: 1, surname: 'Уденцов', name: 'Андрей', position: 'Младший разработчик' }
     ];
 
-let massStatus = {
-    '1' : 'Назначен',
+let status = {
+    '1' : 'Назначена',
     '2' : 'В работе',
-    '3' : 'Выполнен'
+    '3' : 'Выполнен',
+    '4' : 'Отменена'
 };
