f7c0e51fd791eb15a21669610623d952e77ad351
Andrej1405 <andrej.udentsov@yandex.ru>
2020-04-06 11:00:39 +0400

    add revel

--- webix/script/data.js
+++
@@ -1,6 +0,0 @@
-let massProduct = [
-    { id: 1, name: 'Автомобиль', quantity: 5, price: 1000 },
-    { id: 2, name: 'Паровоз', quantity: 10, price: 2000  },
-    { id: 3, name: 'Теплоход', quantity: 15, price: 3000  }
-],
-    basketProduct = [];
